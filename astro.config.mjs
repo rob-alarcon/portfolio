7c66e0136cb709aa121bcbbed3617722ad7bfa3d
robalarcon <rob.alarcon@gmail.com>
2023-07-12 17:15:04 -0700

    add google tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown()],
   vite: {
     ssr: {
-      external: ['svgo'],
-    },
-  },
+      external: ['svgo']
+    }
+  }
 });
